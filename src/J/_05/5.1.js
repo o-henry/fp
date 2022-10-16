6513e4ecf8df91edb2d1792b1d79c308ed6fb42b
henry <cksgod16@gmail.com>
2022-10-16 22:56:21 +0900

    update

--- src/J/_05/5.1.js
+++ src/J/_05/5.1.js
@@ -1,3 +1,4 @@
+import R from "rambda";
 /**
  * handling error
  * mapping an operation to container
@@ -5,6 +6,7 @@
 
 try {
     // 예외가 날 가능성 있는 코드 : 잠재적 위험성이 존재하는 코드
+    var student = findStudent("777-7777-7777");
 } catch (e) {
     // 예외 처리 구문
     console.error(e.message);
--- src/Z/01.ts
+++ src/Z/01.ts
@@ -1,3 +1,4 @@
 // https://github.com/danieljharvey/functional-programming-is-boring/blob/master/slides/lesson1-option.md
+// https://www.educative.io/courses/functional-programming-patterns-with-ramdajs/YQV9QG6gqz9
 
 export {};
